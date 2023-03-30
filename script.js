356a83bf8e22174a84aafa1e72b8cbce81c7ea7c
Greattie <gamubode@gmail.com>
2023-03-30 09:26:05 +0100

    fixed styling issue

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const modal = document.querySelector('.modal-overlay')
 const btn = document.querySelector('.btn')
 const btnModal = document.querySelector('.btn-modal')
 
-console.log(btn)
-
 btn.addEventListener('click', function(){
    modal.classList.add('show-modal')
 
